07cc4abd6ee623717c4762365d0798b903b45d52
VassiliyZh <111jvv@mail.ru>
2022-12-20 22:59:18 +0500

    Выводим среднюю цифру с трехзначного числа

---
+++ example1_letter_in_center/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вводим трехзначное число, выводим среднюю цифру
+Console.WriteLine("Введите трехзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstNumber = number / 10;
+int secondNumber =  firstNumber % 10;
+
+System.Console.WriteLine(secondNumber);
