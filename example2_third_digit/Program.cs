4d8675a4a8cf58c3cfa6a3b005156f0b7f838df1
VassiliyZh <111jvv@mail.ru>
2022-12-20 23:06:35 +0500

    Выводим последнюю цифру с трехначного числа

---
+++ example2_third_digit/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вводим трехзначное число, выводим последнюю цифру или сообщает об ее отсутствии
+Console.WriteLine("Введите трехзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstNumber = number / 100;
+if (firstNumber == 0)
+{
+    System.Console.WriteLine("Ввели не трехначное число");
+}
+else
+{
+    int secondNumber =  number % 10;
+    System.Console.WriteLine(secondNumber);
+}
