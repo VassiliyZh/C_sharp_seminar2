f70a928429c7db0c7e5ee0e9244a00c270c2c337
VassiliyZh <111jvv@mail.ru>
2022-12-20 23:40:57 +0500

    Выводим дни недели по числам

---
+++ example3_days_week/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вводим трехзначное число, выводим последнюю цифру или сообщает об ее отсутствии
+Console.WriteLine("Введите число от 1 до 7 для вывода выходного дня недели");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number == 6 || number == 7)
+{
+    System.Console.WriteLine($"{number} это число является днем недели выходного дня");
+}
+if (number < 1 || number > 7)
+{
+    System.Console.WriteLine($"{number} это число не является днем недели");
+}
+if (number >= 1 && number <= 5)
+{
+    System.Console.WriteLine($"{number} это число является днем недели");
+}
